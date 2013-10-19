d40b749c49a8036922c45c24c76d6e5e105d68f8
Brandyn A. White <bwhite@dappervision.com>
2013-10-19 19:13:57 -0400

    Changed over to new msgpack format for Server/Webapp
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- server/websockets.go
+++ server/websockets.go
@@ -6,53 +6,18 @@ import (
 	"github.com/ugorji/go/codec"
 	"fmt"
 	"strings"
+	"encoding/json"
 	"time"
 )
 
-type WSSensor struct {
-	Timestamp    float64   `json:"timestamp"`
-	TimestampRaw int64     `json:"timestampRaw"`
-	Type         int       `json:"type"`
-	Name         string    `json:"name"`
-	Values       []float64 `json:"values"`
-}
-
-type WSAnnotation struct {
-	Timestamp float64 `json:"timestamp"`
-	Name      string  `json:"name"`
-	Polarity  bool    `json:"polarity"`
-}
-
-type WSData struct {
-	// See Hacking.md for details
-	Tsave     float64              `json:"Tsave,omitempty"` // Time packet data is final
-	Tg0       float64              `json:"Tg0,omitempty"`   // Time packet is saved
-	Ts0       float64              `json:"Ts0,omitempty"`
-	Tg1       float64              `json:"Tg1,omitempty"`
-	Sensors   []WSSensor           `json:"sensors"`
-	Script    string               `json:"script"`
-	ScriptUrl string               `json:"scriptUrl"`
-	Message   string               `json:"message"`
-	Imageb64  *string              `json:"imageb64,omitempty"`
-	Action    string               `json:"action,omitempty"`
-	Timestamp float64              `json:"timestamp,omitempty"`
-	GlassID   string               `json:"glassID,omitempty"`
-	H         []float64            `json:"H,omitempty"`
-	MatchKey  string               `json:"matchKey,omitempty"`
-	Flags     []string             `json:"flags,omitempty"`
-	Say       *string              `json:"say,omitempty"`
-	Draw      [][]interface{}      `json:"draw,omitempty"`
-	Ti        *mirror.TimelineItem `json:"ti,omitempty"`
-}
-
-var DeviceChannels = map[string][]chan *WSData{} // [user]
-var WebChannels = map[string][]chan *WSData{}    // [user]
+var DeviceChannels = map[string][]chan *[]interface{}{} // [user]
+var WebChannels = map[string][]chan *[]interface{}{}    // [user]
 
 func CurTime() float64 {
 	return float64(time.Now().UnixNano()) / 1000000000.
 }
 
-func WSSendWeb(userId string, data *WSData) error {
+func WSSendWeb(userId string, data *[]interface{}) error {
 	for _, c := range WebChannels[userId] {
 		select {
 		case c <- data:
@@ -64,7 +29,7 @@ func WSSendWeb(userId string, data *WSData) error {
 	return nil
 }
 
-func WSSendDevice(userId string, data *WSData) error {
+func WSSendDevice(userId string, data *[]interface{}) error {
 	for _, c := range DeviceChannels[userId] {
 		select {
 		case c <- data:
@@ -79,6 +44,7 @@ func MsgpackMarshal(v interface{}) (data []byte, payloadType byte, err error) {
 	mh := codec.MsgpackHandle{}
 	enc := codec.NewEncoderBytes(&data, &mh)
 	err = enc.Encode(v)
+	//fmt.Println("Marshal: " + B64Enc(string(data)))
 	payloadType = websocket.BinaryFrame
 	return
 }
@@ -90,7 +56,7 @@ func MsgpackUnmarshal(data []byte, payloadType byte, v interface{}) (err error) 
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(B64Enc(string(data)))
+	//fmt.Println("Unmarshal: " + B64Enc(string(data)))
 	return
 }
 
@@ -119,12 +85,11 @@ func WSGlassHandler(c *websocket.Conn) {
 		return
 	}
 	// TODO: Look into locking and add defer to cleanup later, make buffer size configurable
-	wsSendChan := make(chan *WSData, 5)
+	wsSendChan := make(chan *[]interface{}, 5)
 	DeviceChannels[userId] = append(DeviceChannels[userId], wsSendChan)
 
 	// Initialize delays
 	die := false
-	sensorCache := map[int]*WSSensor{}
 
 	// Websocket sender
 	go func() {
@@ -137,7 +102,7 @@ func WSGlassHandler(c *websocket.Conn) {
 			fmt.Println("Sending to glass")
 			fmt.Println(request)
 			msgcodec := websocket.Codec{MsgpackMarshal, MsgpackUnmarshal}
-			err = msgcodec.Send(c, *request)
+			err = msgcodec.Send(c, request)
 			if err != nil {
 				die = true
 				break
@@ -159,21 +124,18 @@ func WSGlassHandler(c *websocket.Conn) {
 				continue
 			}
 			fmt.Println("Sending flags")
-			wsSendChan <- &WSData{Action: "flags", Flags: uflags}
+			wsSendChan <- &[]interface{}{"flags", uflags}
 			time.Sleep(time.Millisecond * 2000)
 		}
 	}()
 
 	// Data from glass loop
-	skew := 0.
-	delay := 0.
-	delayData := 0.
-	cnt := 0
 	for {
-		request := WSData{}
+		request := []interface{}{}
 		msgcodec := websocket.Codec{MsgpackMarshal, MsgpackUnmarshal}
 		err := msgcodec.Receive(c, &request)
 		if err != nil {
+			LogPrintf("ws: from glass")
 			fmt.Println(err)
 			die = true
 			break
@@ -181,43 +143,25 @@ func WSGlassHandler(c *websocket.Conn) {
 		if die {
 			break
 		}
-		fmt.Println(fmt.Sprintf("Send Delay[%f] (not deskewed)", CurTime()-request.Tg0))
-		cnt += 1
-		fmt.Println(request.Action)
-		if request.Action == "timeline" {
-			fmt.Println(request)
-			req := svc.Timeline.Insert(request.Ti)
+		action := string(request[0].([]uint8))
+		fmt.Println(action)
+		if action == "timeline" {
+			ti := mirror.TimelineItem{}
+			err = json.Unmarshal(request[1].([]byte), &ti)
+			if err != nil {
+				LogPrintf("ws: timeline: js")
+				continue
+			}
+			req := svc.Timeline.Insert(&ti)
 			//req.Media(strings.NewReader(image))
 			_, err := req.Do()
 			if err != nil {
-				LogPrintf("ws: timeline")
+				LogPrintf("ws: timeline: send")
 				continue
 			}
-		} else if request.Action == "log" {
-			fmt.Println(request)
+		} else if action == "log" {
+			fmt.Println(request[1].(string))
 			WSSendWeb(userId, &request)
-		} else if request.Action == "pong" {
-			Ts1 := CurTime()
-			delay = .5 * (Ts1 - request.Ts0)
-			skew = request.Tg1 - Ts1 + delay
-			delayData = request.Ts0 + skew - request.Tg0
-			fmt.Println(fmt.Sprintf("Tg0[%f] -> D0[%f] -> Ts0[%f]", request.Tg0, delayData, request.Ts0))
-			fmt.Println(fmt.Sprintf("Tg1[%f] <- D[%f] <- Ts0[%f]", request.Tg1, delay, request.Ts0))
-			fmt.Println(fmt.Sprintf("Tg1[%f] -> D[%f] -> Ts1[%f]", request.Tg1, delay, Ts1))
-			fmt.Println(fmt.Sprintf("Skew[%f] Deskewed times (server centric, origin at glass send)", skew))
-			origin := request.Tg0 - skew
-			fmt.Println(fmt.Sprintf("Tg0[%f] -> D0[%f] -> Ts0[%f]", request.Tg0-skew-origin, delayData, request.Ts0-origin))
-			fmt.Println(fmt.Sprintf("Tg1[%f] <- D[%f] <- Ts0[%f]", request.Tg1-skew-origin, delay, request.Ts0-origin))
-			fmt.Println(fmt.Sprintf("Tg1[%f] -> D[%f] -> Ts1[%f]", request.Tg1-skew-origin, delay, Ts1-origin))
-		} else if request.Action == "data" {
-			request.Timestamp = request.Tsave - skew
-			for _, sensor := range request.Sensors {
-				sensorCache[sensor.Type] = &sensor
-			}
-			WSSendWeb(userId, &request)
-			if request.Imageb64 != nil {
-				wsSendChan <- &WSData{Action: "ping", Tg0: request.Tg0, Ts0: CurTime()}
-			}
 		} else {
 			WSSendWeb(userId, &request)
 		}
@@ -242,7 +186,7 @@ func WSWebHandler(c *websocket.Conn) {
 	fmt.Println("Websocket connected")
 	// TODO: Look into locking and add defer to cleanup later, make buffer size configurable
 	// TODO: This needs the "die" code added, look into glass side also
-	wsSendChan := make(chan *WSData, 5)
+	wsSendChan := make(chan *[]interface{}, 5)
 	WebChannels[userId] = append(WebChannels[userId], wsSendChan)
 	// Websocket sender
 	go func() {
@@ -260,14 +204,16 @@ func WSWebHandler(c *websocket.Conn) {
 	}()
 	// Data from web loop
 	for {
-		request := WSData{}
+		request := []interface{}{}
 		msgcodec := websocket.Codec{MsgpackMarshal, MsgpackUnmarshal}
 		err := msgcodec.Receive(c, &request)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		if request.Action == "sendTimelineImage" {
+		action := string(request[0].([]uint8))
+		fmt.Println(action)
+		if action == "sendTimelineImage" {
 			trans := authTransport(userId)
 			if trans == nil {
 				LogPrintf("notify: auth")
@@ -278,14 +224,14 @@ func WSWebHandler(c *websocket.Conn) {
 				LogPrintf("notify: mirror")
 				continue
 			}
-			sendImageCard(B64Dec(*request.Imageb64), "", svc)
-		} else if request.Action == "signScript" {
-			scriptBytes, err := SignatureSignScript(userId, []byte(request.Script))
+			sendImageCard(request[1].(string), "", svc)
+		} else if action == "signScript" {
+			scriptBytes, err := SignatureSignScript(userId, request[1].([]byte))
 			if err != nil {
 				LogPrintf("notify: request")
 				continue
 			}
-			request.Script = string(scriptBytes)
+		    request[1] = scriptBytes
 			wsSendChan <- &request
 		} else {
 			WSSendDevice(userId, &request)
