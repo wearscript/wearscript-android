0d335713e1a96e4f8385ecb7b301649c76845667
Brandyn A. White <bwhite@dappervision.com>
2013-10-21 18:43:07 -0400

    Added msgpack codec to install.sh and changed default sampling rate for local storage in example script
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- server/install.sh
+++ server/install.sh
@@ -4,4 +4,5 @@ go get -u code.google.com/p/google-api-go-client/mirror/v1
 go get -u github.com/garyburd/redigo/redis
 go get -u github.com/gorilla/pat
 go get -u github.com/gorilla/sessions
+go get -u github.com/ugorji/go/codec
 go build
