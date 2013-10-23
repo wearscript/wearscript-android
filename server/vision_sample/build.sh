4cfa37d1f2cf105a820d8c85a918d7e57bb82e24
Brandyn A. White <bwhite@dappervision.com>
2013-10-23 03:35:02 -0400

    Move library to /usr/lib
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- server/vision_sample/build.sh
+++ server/vision_sample/build.sh
@@ -1,2 +1,3 @@
 gcc -fPIC -shared -Wl,-soname,libvision_sample.so -o libvision_sample.so vision_sample.c -lopencv_core -lopencv_highgui -Wall -O3 -DUSE_SAMPLE
 gcc -fPIC main_test.c vision_sample.c -lopencv_core -lopencv_highgui -Wall -o main_test -O3 -DUSE_SAMPLE
+mv libvision_sample.so /usr/lib/
