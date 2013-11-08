99f47a28cce19fcc988c22dd72dc617176347009
Scott Greenwald <swgreen@mit.edu>
2013-11-08 05:29:05 -0500

    Clear cache when new webviews are instantiated.
    This might have some disadvantages, but the advantage is that
    changes to your included javascript files get loaded. Else you're
    stuck with the first version you push/load for a while.

--- glass/WearScript/src/main/java/com/dappervision/wearscript/BackgroundService.java
+++ glass/WearScript/src/main/java/com/dappervision/wearscript/BackgroundService.java
@@ -472,6 +472,7 @@ public class BackgroundService extends Service implements AudioRecord.OnRecordPo
         synchronized (lock) {
             // TODO(brandyn): Refactor these as they are similar
             webview = new WebView(this);
+            webview.clearCache(true);
             webview.setWebChromeClient(new WebChromeClient() {
                 public boolean onConsoleMessage(ConsoleMessage cm) {
                     String msg = cm.message() + " -- From line "
@@ -498,6 +499,7 @@ public class BackgroundService extends Service implements AudioRecord.OnRecordPo
         synchronized (lock) {
             // TODO(brandyn): Refactor these as they are similar
             webview = new WebView(this);
+            webview.clearCache(true);
             webview.setWebChromeClient(new WebChromeClient() {
                 public boolean onConsoleMessage(ConsoleMessage cm) {
                     String msg = cm.message() + " -- From line "
