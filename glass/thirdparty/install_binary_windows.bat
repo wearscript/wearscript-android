e7c455e14a6b410fe5324d5bb80b51cd956c04ec
Justin Chase <jujowned@Gmail.com>
2013-11-21 09:57:20 -0800

    Updated windows install batch to include new path to the adb.exe
    Someone added some binaries for adb to the thirdparty directory. It makes sense to just have the windows users now utilize this than ask them to create path variables.

--- glass/thirdparty/install_binary_windows.bat
+++ glass/thirdparty/install_binary_windows.bat
@@ -1,4 +1,4 @@
-adb install OpenCV_2.4.6_Manager_2.9_armv7a-neon.apk
-adb install launchy.apk
-adb install CaptureActivity.apk
-adb install wearscript.apk
+adb\windows\adb.exe install OpenCV_2.4.6_Manager_2.9_armv7a-neon.apk
+adb\windows\adb.exe install launchy.apk
+adb\windows\adb.exe install CaptureActivity.apk
+adb\windows\adb.exe install wearscript.apk
