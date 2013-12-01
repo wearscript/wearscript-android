672d1f390a8cd4011354c7f5c9a66906f02701b5
Brandyn A. White <bwhite@dappervision.com>
2013-12-01 15:06:13 -0500

    Fixed environment setup Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- vagrant/manifests/init.pp
+++ vagrant/manifests/init.pp
@@ -2,10 +2,6 @@ group { "puppet":
     ensure => "present",
 }
 
-exec { 'setenvironment':
-command => "/bin/bash -c 'printf GOPATH=`pwd`/gocode\\\\nPATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:`pwd`/android-studio/sdk/platform-tools:`pwd`/android-studio/sdk/tools' > /etc/environment",
-}
-
 class apt_packages {
   exec { "apt-get-update":
     command => "/usr/bin/apt-get update",
@@ -23,10 +19,10 @@ class pip_packages {
       package {
       "gevent":
           ensure => latest,
-	  provider => pip;
+          provider => pip;
       "gevent-websocket":
           ensure => latest,
-	  require => Package["gevent"],
+          require => Package["gevent"],
           provider => pip;
       "redis":
           ensure => latest,
@@ -43,14 +39,11 @@ class pip_packages {
       }      
 }
 
-class go_packages {
-  exec { "go-dependencies":
-    command => "/bin/mkdir -p /home/vagrant/gocode && /usr/bin/go get -u code.google.com/p/go.net/websocket code.google.com/p/goauth2/oauth code.google.com/p/google-api-go-client/mirror/v1 github.com/garyburd/redigo/redis github.com/gorilla/pat github.com/gorilla/sessions github.com/ugorji/go/codec",
-    environment => ["GOPATH=/home/vagrant/gocode"]
-  }   
+class java {
+exec { 'setenvironment':
+command => "/bin/bash -c 'echo GOPATH=`pwd`/gocode > /etc/environment && echo PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:`pwd`/android-studio/sdk/platform-tools:`pwd`/android-studio/sdk/tools' >> /etc/environment",
 }
 
-class java {
   package { "python-software-properties": }
  
   exec { "add-apt-repository-oracle":
@@ -68,25 +61,21 @@ class java {
 
 class wearscript {
   exec { "clone-repo":
-    command => "/usr/bin/git clone https://github.com/OpenShades/wearscript.git && /bin/chown -R vagrant:vagrant wearscript",
-    cwd => '/home/vagrant'
+    command => "/usr/bin/git clone https://github.com/OpenShades/wearscript.git && chown --reference . -R wearscript",
   }
 }
 
 class android_studio {
   exec { "download-android-studio":
-    command => "/usr/bin/wget https://dl-ssl.google.com/dl/android/studio/install/0.3.2/android-studio-bundle-132.893413-linux.tgz && /bin/tar -xzf android-studio-bundle-132.893413-linux.tgz && /bin/chown -R vagrant:vagrant android-studio",
-    creates => '/home/vagrant/android-studio',
-    cwd => '/home/vagrant',
+    command => "/usr/bin/wget https://dl-ssl.google.com/dl/android/studio/install/0.3.2/android-studio-bundle-132.893413-linux.tgz && /bin/tar -xzf android-studio-bundle-132.893413-linux.tgz && chown --reference . -R android-studio",
     timeout => 3600
   }
 }
 
 stage {[java, install_apt, install_pip, install_go, install_wearscript]:}
-Stage[java] -> Stage[install_apt] -> Stage[install_pip] -> Stage[install_go] -> Stage[install_wearscript]
+Stage[java] -> Stage[install_apt] -> Stage[install_pip] -> Stage[install_wearscript]
 class {java: stage => java}
 class {apt_packages: stage => install_apt}
 class {pip_packages: stage => install_pip}
-class {go_packages: stage => install_go}
 class {wearscript: stage => install_wearscript}
 class {android_studio: stage => install_wearscript}
