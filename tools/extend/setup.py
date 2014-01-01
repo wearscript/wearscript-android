514852954d8055d9158328fd483261a539b32503
lrvick <lance@lrvick.net>
2014-01-01 13:47:46 -0500

    initial wearscript command line client

--- tools/extend/setup.py
+++ tools/extend/setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-
 setup(name='wearscript',
       version='0.0.1',
       packages=['wearscript'],
+      scripts=['bin/wearscript'],
       author='Brandyn A. White',
       author_email='bwhite@dappervision.com',
       license='Apache 2.0')
