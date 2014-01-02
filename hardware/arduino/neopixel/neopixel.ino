f4352e18bab4f9b2be9cf042e93ebd24d4b7864b
Brandyn A. White <bwhite@dappervision.com>
2014-01-02 12:47:58 -0500

    Updated neopixel code with buffering data and loops
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- hardware/arduino/neopixel/neopixel.ino
+++ hardware/arduino/neopixel/neopixel.ino
@@ -9,8 +9,11 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(17, PIN, NEO_GRB + NEO_KHZ800);
-int data[5];
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
+uint8_t *data;
+uint8_t loop_data[2560];
+int loop_pos = 0;
+int loop_state = 0; // 0: no loop, 1: saving loop data, 2: looping
 int i;
 void setup() {
   strip.begin();
@@ -19,14 +22,44 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available() < 5)
-    return;
-  for (i = 0; i < 5; i++)
-      data[i] = Serial.read();
-  if (data[0] == 0)
+  data = loop_data + loop_pos;
+  if (Serial.available() < 6) {
+    if (loop_state != 2)
+      return;
+  } else {
+      if (loop_state == 2) {
+          // If data comes in while we loop, clear the loop
+          loop_state = 0;
+	  loop_pos = 0;
+      }
+      data[0] = Serial.read();
+      if (data[0] != 255)
+          return;
+      for (i = 0; i < 5; i++)
+          data[i] = Serial.read();
+  }
+  if (loop_state != 0)
+      loop_pos += 5;
+  switch (data[0]) {
+    case 0:
       strip.setPixelColor(data[1], data[2], data[3], data[4]);
-  else if (data[0] == 1)
+      break;
+    case 1:
       strip.setBrightness(data[1]);
-  else if (data[0] == 2)
+      break;
+    case 2:
       strip.show();
+      break;
+    case 3:
+      delay(data[1]);
+      break;
+    case 4: // start loop
+      loop_state = 1;
+      loop_pos = 0;
+      break;
+    case 5: // finish loop
+      loop_state = 2;
+      loop_pos = 0;
+      break;
+  }
 }
--- hardware/arduino/neopixel/neopixel.py
+++ hardware/arduino/neopixel/neopixel.py
@@ -1,24 +1,39 @@
 import serial
+import time
 
 SERIAL = None
 def command(serialport, *args):
     global SERIAL
     if SERIAL is None:
         SERIAL = serial.Serial(serialport, 9600, timeout=1)
-    for arg in args:
-        SERIAL.write(chr(arg))
+    print(args)
+    for a in args:
+        assert 0 <= a < 256
+        SERIAL.write(chr(a))
 
 if __name__ == '__main__':
     import argparse
-    import json
+    import msgpack
+    import base64
     import wearscript
+    if 0:
+        #payload = [255, 0, 0, 0, 0, 0,  255, 0, 0, 255, 255, 0, 255, 2, 0, 255, 255, 0];
+        kw = {'serialport': '/dev/ttyACM0'}
+        for i in range(len(payload) / 6):
+            command(kw['serialport'], *payload[i * 6: (i + 1) * 6])
+            time.sleep(.5)
+        time.sleep(10)
+        quit()
     def callback(ws, **kw):
         print('Listening...')
         while 1:
             data = ws.receive()
             if data[0] == 'blob' and data[1] == 'arduino':
                 print(data)
-                command(kw['serialport'], *json.loads(data[2]))
+                payload = msgpack.loads(base64.b64decode(data[2]))
+                print(payload)
+                for i in range(len(payload) / 6):
+                    command(kw['serialport'], *payload[i * 6: (i + 1) * 6])
     parser = argparse.ArgumentParser()
     parser.add_argument('--serialport', default='/dev/ttyACM0')
     wearscript.parse(callback, parser)
