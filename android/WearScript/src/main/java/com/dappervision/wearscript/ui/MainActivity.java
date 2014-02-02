e7ffc73fae87b19e66e8a93d7b1f281bc2c98a17
Scott Greenwald <swgreen@mit.edu>
2014-02-02 13:13:26 -0700

    Native Glass menu for launching script activities on Glass
    For now the ListView is still in the background.
    Also, for some reason the menu doesn't open when I install
    and launchy from Android Studio, but does when I launch
    from Glass.

--- android/WearScript/src/main/java/com/dappervision/wearscript/ui/MainActivity.java
+++ android/WearScript/src/main/java/com/dappervision/wearscript/ui/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 
+import com.dappervision.wearscript.HardwareDetector;
 import com.dappervision.wearscript.R;
 import com.dappervision.wearscript.WearScriptInfo;
 
 public class MainActivity extends FragmentActivity implements ScriptListFragment.Callbacks {
+    private Menu mMenu;
+
     protected ScriptListFragment createFragment() {
         return ScriptListFragment.newInstance();
     }
@@ -21,6 +26,18 @@ public class MainActivity extends FragmentActivity implements ScriptListFragment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResId());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        mMenu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        onScriptSelected(getFragment().getWearScriptInfo(item.getItemId()));
+        return true;
     }
 
     protected int getLayoutResId() {
@@ -35,11 +52,19 @@ public class MainActivity extends FragmentActivity implements ScriptListFragment
         manager.beginTransaction()
                 .replace(R.id.fragmentContainer, fragment)
                 .commit();
+        if (HardwareDetector.isGlass) {
+            openOptionsMenu();
+        }
     }
 
     @Override
     public void onScriptSelected(WearScriptInfo scriptInfo) {
         startActivity(scriptInfo.getIntent());
+    }
+
+    @Override
+    public Menu getOptionsMenu() {
+        return mMenu;
     }
 
     @Override
