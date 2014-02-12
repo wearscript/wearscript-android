131e7592ea340534aadd5d806d9ad206e93d22dc
Brandyn A. White <bwhite@dappervision.com>
2014-02-11 20:33:08 -0500

    Added one line installer
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'WearScript'
-copyright = u'2013, Dapper Vision, Inc.'
+copyright = u'2014, Dapper Vision, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
