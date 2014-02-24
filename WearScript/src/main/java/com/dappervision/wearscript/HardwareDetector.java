1a041c0a6c706f6df802f15b4abd4c51039a8753
Kurt Nelson <kurtisnelson@gmail.com>
2014-02-23 22:58:17 -0500

    Warn user when GDK is expected and not present

--- WearScript/src/main/java/com/dappervision/wearscript/HardwareDetector.java
+++ WearScript/src/main/java/com/dappervision/wearscript/HardwareDetector.java
@@ -4,4 +4,5 @@ import android.os.Build;
 
 public class HardwareDetector {
     public static final boolean isGlass = Build.PRODUCT.equals("glass_1");
+    public static final boolean hasGDK = isGlass;
 }
