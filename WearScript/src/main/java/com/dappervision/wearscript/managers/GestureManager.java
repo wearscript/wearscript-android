e4db71b30594949a8d625c082712093bb2c5ca20
Kurt Nelson <kurtisnelson@gmail.com>
2014-02-28 16:25:26 -0500

    onEyeGesture events

--- WearScript/src/main/java/com/dappervision/wearscript/managers/GestureManager.java
+++ WearScript/src/main/java/com/dappervision/wearscript/managers/GestureManager.java
@@ -1,37 +1,88 @@
 package com.dappervision.wearscript.managers;
 
 import android.content.Context;
+import android.content.IntentFilter;
 import android.view.MotionEvent;
 
 import com.dappervision.wearscript.BackgroundService;
+import com.dappervision.wearscript.Log;
+import com.dappervision.wearscript.dataproviders.EyeEventReceiver;
+import com.google.android.glass.eye.EyeGesture;
+import com.google.android.glass.eye.EyeGestureManager;
 import com.google.android.glass.touchpad.Gesture;
 import com.google.android.glass.touchpad.GestureDetector;
 
 public class GestureManager extends Manager {
     private static final String TAG = "GestureManager";
+    private final EyeGestureManager eyeGestureManager;
+    private final EyeEventReceiver eyeEventReceiver;
     private MyGestureDetector detector;
 
     public GestureManager(Context activity, BackgroundService bs) {
         super(bs);
         detector = new MyGestureDetector(this, activity);
+        eyeGestureManager = EyeGestureManager.from(activity);
+        eyeEventReceiver = new EyeEventReceiver(detector);
         reset();
     }
 
     public void onEvent(MotionEvent e) {
         detector.onMotionEvent(e);
     }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+        teardown();
+    }
+
+    @Override
+    public void reset() {
+        super.reset();
+        teardown();
+        setup();
+    }
+
+    private void teardown(){
+        eyeGestureManager.stopDetector(EyeGesture.DOFF);
+        eyeGestureManager.stopDetector(EyeGesture.DON);
+        eyeGestureManager.stopDetector(EyeGesture.WINK);
+        eyeGestureManager.stopDetector(EyeGesture.DOUBLE_WINK);
+        eyeGestureManager.stopDetector(EyeGesture.DOUBLE_BLINK);
+        try{
+        service.getApplicationContext().unregisterReceiver(eyeEventReceiver);
+        }catch (IllegalArgumentException e){
+
+        }
+    }
+
+    private void setup(){
+        eyeGestureManager.enableDetectorPersistently(EyeGesture.WINK, true);
+        eyeGestureManager.enableDetectorPersistently(EyeGesture.DOUBLE_WINK, true);
+        eyeGestureManager.enableDetectorPersistently(EyeGesture.DOUBLE_BLINK, true);
+        eyeGestureManager.enableDetectorPersistently(EyeGesture.DOFF, true);
+        eyeGestureManager.enableDetectorPersistently(EyeGesture.DON, true);
+        IntentFilter eyeFilter = new IntentFilter(
+                "com.google.glass.action.EYE_GESTURE");
+        eyeFilter.setPriority(3000);
+        service.getApplicationContext().registerReceiver(eyeEventReceiver, eyeFilter);
+    }
 }
 
-class MyGestureDetector extends GestureDetector implements GestureDetector.BaseListener, GestureDetector.FingerListener, GestureDetector.ScrollListener, GestureDetector.TwoFingerScrollListener {
+class MyGestureDetector extends GestureDetector implements EyeEventReceiver.BaseListener, GestureDetector.BaseListener, GestureDetector.FingerListener, GestureDetector.ScrollListener, GestureDetector.TwoFingerScrollListener {
+    private static final String TAG = "MyGestureDetector";
+    private final EyeEventReceiver eyeEventReciever;
     private GestureManager parent;
 
     MyGestureDetector(GestureManager parent, Context context) {
         super(context);
         this.parent = parent;
+        this.eyeEventReciever = new EyeEventReceiver(this);
         setBaseListener(this);
         setFingerListener(this);
         setScrollListener(this);
         setTwoFingerScrollListener(this);
+
     }
 
     @Override
@@ -56,4 +107,12 @@ class MyGestureDetector extends GestureDetector implements GestureDetector.BaseL
         parent.makeCall("onTwoFingerScroll", String.format("%f, %f, %f", v, v2, v3));
         return false;
     }
+
+    @Override
+    public void onEyeGesture(EyeGesture gesture) {
+        parent.makeCall("onEyeGesture", String.format("'%s'", gesture.name()));
+    }
 }
+
+
+
