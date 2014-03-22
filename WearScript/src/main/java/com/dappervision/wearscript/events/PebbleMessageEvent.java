ae072596ed7df4532d3be7fa29032bdf543fa0c3
connerbrooks <connerbrooks@gmail.com>
2014-03-22 13:56:15 -0500

    Add required attributes for setting text and sending vibrations

--- WearScript/src/main/java/com/dappervision/wearscript/events/PebbleMessageEvent.java
+++ WearScript/src/main/java/com/dappervision/wearscript/events/PebbleMessageEvent.java
@@ -1,7 +1,35 @@
 package com.dappervision.wearscript.events;
 
-/**
- * Created by conner on 3/18/14.
- */
 public class PebbleMessageEvent {
+    private String type;
+    private String text;
+    private boolean clear;
+    private int vibeType;
+
+    public PebbleMessageEvent(String type, String text, boolean clear) {
+        this.type = type;
+        this.text = text;
+        this.clear = clear;
+    }
+
+    public PebbleMessageEvent(String type, int vibeType){
+        this.type = type;
+        this.vibeType = vibeType;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean getClear() {
+        return clear;
+    }
+
+    public int getVibeType() {
+        return vibeType;
+    }
 }
