87970da2b9094f700250ea5a2db3698c4051f4c8
Kurt Nelson <kurtisnelson@gmail.com>
2014-03-25 12:40:49 -0400

    Integrate HandlerHandlers into the main app

--- WearScript/src/main/java/com/dappervision/wearscript/handlers/HandlerHandler.java
+++ WearScript/src/main/java/com/dappervision/wearscript/handlers/HandlerHandler.java
@@ -3,16 +3,36 @@ package com.dappervision.wearscript.handlers;
 import java.util.ArrayList;
 
 public class HandlerHandler {
+    private static HandlerHandler singleton;
     ArrayList<Handler> handlers;
 
     public HandlerHandler() {
         handlers = new ArrayList<Handler>();
+        newHandlers();
     }
 
-    public void shutdown() {
+    public static HandlerHandler get() {
+        if (singleton != null) {
+            return singleton;
+        }
+        singleton = new HandlerHandler();
+        return singleton;
+    }
+    public void resetAll() {
+        for (Handler h : handlers) {
+            h.shutdown();
+        }
+        newHandlers();
+    }
+
+    public void shutdownAll() {
         for (Handler h : handlers) {
             h.shutdown();
         }
         handlers = null;
+    }
+
+    public void newHandlers() {
+
     }
 }
